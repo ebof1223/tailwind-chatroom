eeccc5a4d664efc51741d68a37de2ffd7f927b31
ebof1223 <erwinjbofetiado@gmail.com>
2023-08-18 21:54:54 -0500

    ui and fxing socket

--- src/client/App.tsx
+++ src/client/App.tsx
@@ -1,34 +1,184 @@
-import "./App.css";
-
+import { io } from "socket.io-client";
 import { useState } from "react";
 
-import reactLogo from "./assets/react.svg";
-
 function App() {
-  const [count, setCount] = useState(0);
-
+  const socket = io("http://localhost:3000");
+  socket.on("connect", () => {
+    console.log("connected");
+  });
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className="flex-1 p-2 sm:p-6 justify-between flex flex-col h-screen">
+      <div className="flex sm:items-center justify-between py-3 border-b-2 border-gray-200">
+        <div className="relative flex items-center space-x-4">
+          <div className="relative">
+            <span className="absolute text-green-500 right-0 bottom-0">
+              <svg width="20" height="20">
+                <circle cx="8" cy="8" r="8" fill="currentColor"></circle>
+              </svg>
+            </span>
+            <img
+              src="https://images.unsplash.com/photo-1549078642-b2ba4bda0cdb?ixlib=rb-1.2.1&amp;ixid=eyJhcHBfaWQiOjEyMDd9&amp;auto=format&amp;fit=facearea&amp;facepad=3&amp;w=144&amp;h=144"
+              alt=""
+              className="w-10 sm:w-16 h-10 sm:h-16 rounded-full"
+            />
+          </div>
+          <div className="flex flex-col leading-tight">
+            <div className="text-2xl mt-1 flex items-center">
+              <span className="text-gray-700 mr-3">Anderson Vanhron</span>
+            </div>
+            <span className="text-lg text-gray-600">Junior Developer</span>
+          </div>
+        </div>
+        <div className="flex items-center space-x-2">
+          <button
+            type="button"
+            className="inline-flex items-center justify-center rounded-lg border h-10 w-10 transition duration-500 ease-in-out text-gray-500 hover:bg-gray-300 focus:outline-none"
+          >
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+              className="h-6 w-6"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
+              ></path>
+            </svg>
+          </button>
+          <button
+            type="button"
+            className="inline-flex items-center justify-center rounded-lg border h-10 w-10 transition duration-500 ease-in-out text-gray-500 hover:bg-gray-300 focus:outline-none"
+          >
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+              className="h-6 w-6"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z"
+              ></path>
+            </svg>
+          </button>
+          <button
+            type="button"
+            className="inline-flex items-center justify-center rounded-lg border h-10 w-10 transition duration-500 ease-in-out text-gray-500 hover:bg-gray-300 focus:outline-none"
+          >
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+              className="h-6 w-6"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M15 17h5l-1.405-1.405A2.032 2.032 0 0118 14.158V11a6.002 6.002 0 00-4-5.659V5a2 2 0 10-4 0v.341C7.67 6.165 6 8.388 6 11v3.159c0 .538-.214 1.055-.595 1.436L4 17h5m6 0v1a3 3 0 11-6 0v-1m6 0H9"
+              ></path>
+            </svg>
+          </button>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
+      <div
+        id="messages"
+        className="h-full my-2 flex flex-col space-y-4 p-3 overflow-y-auto scrollbar-thumb-blue scrollbar-thumb-rounded scrollbar-track-blue-lighter scrollbar-w-2 scrolling-touch"
+      >
+        {/* Message from another user */}
+        <div className="chat-message">
+          <div className="flex items-end">
+            <div className="flex flex-col space-y-2 text-xs max-w-xs mx-2 order-2 items-start">
+              <div>
+                <span className="px-4 py-2 rounded-lg inline-block bg-gray-300 text-gray-600">
+                  Can be verified on any platform using docker
+                </span>
+              </div>
+            </div>
+            <img
+              src="https://images.unsplash.com/photo-1549078642-b2ba4bda0cdb?ixlib=rb-1.2.1&amp;ixid=eyJhcHBfaWQiOjEyMDd9&amp;auto=format&amp;fit=facearea&amp;facepad=3&amp;w=144&amp;h=144"
+              alt="My profile"
+              className="w-6 h-6 rounded-full order-1"
+            />
+          </div>
+        </div>
+
+        {/* Message from current user */}
+        <div className="chat-message">
+          <div className="flex items-end justify-end">
+            <div className="flex flex-col space-y-2 text-xs max-w-xs mx-2 order-1 items-end">
+              <div>
+                <span className="px-4 py-2 rounded-lg inline-block rounded-br-none bg-blue-600 text-white">
+                  Your error message says permission denied, npm global installs
+                  must be given root privileges.
+                </span>
+              </div>
+            </div>
+            <img
+              src="https://images.unsplash.com/photo-1590031905470-a1a1feacbb0b?ixlib=rb-1.2.1&amp;ixid=eyJhcHBfaWQiOjEyMDd9&amp;auto=format&amp;fit=facearea&amp;facepad=3&amp;w=144&amp;h=144"
+              alt="My profile"
+              className="w-6 h-6 rounded-full order-2"
+            />
+          </div>
+        </div>
+
+        {/* ... Add more messages similar to the above format ... */}
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <div className="border-t-2 border-gray-200 px-4 pt-4 mb-2 sm:mb-0">
+        <div className="relative flex">
+          <span className="absolute inset-y-0 flex items-center">
+            <button
+              type="button"
+              className="inline-flex items-center justify-center rounded-full h-12 w-12 transition duration-500 ease-in-out text-gray-500 hover:bg-gray-300 focus:outline-none"
+            >
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+                className="h-6 w-6 text-gray-600"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M19 11a7 7 0 01-7 7m0 0a7 7 0 01-7-7m7 7v4m0 0H8m4 0h4m-4-8a3 3 0 01-3-3V5a3 3 0 116 0v6a3 3 0 01-3 3z"
+                ></path>
+              </svg>
+            </button>
+          </span>
+          <input
+            type="text"
+            placeholder="Write your message!"
+            className="w-full focus:outline-none focus:placeholder-gray-400 text-gray-600 placeholder-gray-600 pl-12 bg-gray-200 rounded-md py-3"
+          />
+          <div className="absolute right-0 items-center inset-y-0 hidden sm:flex">
+            {/* ... same pattern for the rest of the buttons ... */}
+            <button
+              type="button"
+              className="inline-flex items-center justify-center rounded-lg px-4 py-3 transition duration-500 ease-in-out text-white bg-blue-500 hover:bg-blue-400 focus:outline-none"
+            >
+              <span className="font-bold">Send</span>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 20 20"
+                fill="currentColor"
+                className="h-6 w-6 ml-2 transform rotate-90"
+              >
+                <path d="M10.894 2.553a1 1 0 00-1.788 0l-7 14a1 1 0 001.169 1.409l5-1.429A1 1 0 009 15.571V11a1 1 0 112 0v4.571a1 1 0 00.725.962l5 1.428a1 1 0 001.17-1.408l-7-14z"></path>
+              </svg>
+            </button>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
--- src/server/main.ts
+++ src/server/main.ts
@@ -1,12 +1,19 @@
 import express from "express";
 import ViteExpress from "vite-express";
+import { Server } from "socket.io";
 
 const app = express();
 
-app.get("/hello", (_, res) => {
-  res.send("Hello Vite + React + TypeScript!");
+const httpServer = ViteExpress.listen(app, 3000, () =>
+  console.log("Server is listening!")
+);
+
+const io = new Server(httpServer, {
+  cors: { origin: "*" },
 });
 
-ViteExpress.listen(app, 3000, () =>
-  console.log("Server is listening on port 3000...")
-);
+io.on("connection", (socket) => {
+	console.log("server: socket connected")
+});
+
+
